6cbf35c7b2c353ca21967468bf6423e70ed45564
Daniel Gorgan <danut007ro@gmail.com>
2019-05-15 10:24:12 +0300

    Update build script

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ cd ..
 zip -r ./samedaycourier/samedaycourier.zip ./samedaycourier
 cd samedaycourier
 zip --delete samedaycourier.zip "samedaycourier/.git*"
+zip --delete samedaycourier.zip "samedaycourier/config.xml"
+zip --delete samedaycourier.zip "samedaycourier/log/*.log"
