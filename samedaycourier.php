9e38ab553cf04361c736f025fdf5ef5540bd1f45
Daniel Gorgan <danut007ro@gmail.com>
2019-06-08 09:21:01 +0300

    Prepare v1.1.1 release

--- samedaycourier.php
+++ samedaycourier.php
@@ -50,7 +50,7 @@ class SamedayCourier extends CarrierModule
     {
         $this->name = 'samedaycourier';
         $this->tab = 'shipping_logistics';
-        $this->version = '1.1.0';
+        $this->version = '1.1.1';
         $this->author = 'Sameday Courier';
         $this->need_instance = 0;
         $this->bootstrap = true;
