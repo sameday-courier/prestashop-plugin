709a2e95a961ff3f0220f9a6119fb47b99a55b6a
Alexandru Soptareanu <alexandru.soptareanu@sameday.ro>
2021-08-06 09:56:54 +0300

    Remove attribute "required" from "#lockerIdSelector".

--- samedaycourier.php
+++ samedaycourier.php
@@ -103,7 +103,7 @@ class SamedayCourier extends CarrierModule
     {
         $this->name = 'samedaycourier';
         $this->tab = 'shipping_logistics';
-        $this->version = '1.4.6';
+        $this->version = '1.4.7';
         $this->author = 'Sameday Courier';
         $this->need_instance = 0;
         $this->bootstrap = true;
