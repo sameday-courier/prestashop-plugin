b8bc20579aa3a391a23474a3139dc97b148acffe
Alexandru Soptareanu <alexandru.soptareanu@sameday.ro>
2023-05-23 16:12:21 +0300

    Fix compatibility with PS 8.0.* hooks nomenclature.

--- samedaycourier.php
+++ samedaycourier.php
@@ -106,7 +106,7 @@ class SamedayCourier extends CarrierModule
     {
         $this->name = 'samedaycourier';
         $this->tab = 'shipping_logistics';
-        $this->version = '1.5.4';
+        $this->version = '1.5.5';
         $this->author = 'Sameday Courier';
         $this->need_instance = 0;
         $this->bootstrap = true;
