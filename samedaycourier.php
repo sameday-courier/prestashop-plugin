3525501306a90e8fdb857727c6c67a523f63d279
andrei <thesemeth@gmail.com>
2025-09-09 15:34:10 +0300

    updated version

--- samedaycourier.php
+++ samedaycourier.php
@@ -112,7 +112,7 @@ class SamedayCourier extends CarrierModule
         $this->name = 'samedaycourier';
         $this->tab = 'shipping_logistics';
 
-        $this->version = '1.7.5';
+        $this->version = '1.8.1';
         $this->author = 'Sameday Courier';
         $this->need_instance = 0;
         $this->bootstrap = true;
