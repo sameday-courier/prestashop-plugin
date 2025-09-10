de94bc7f300109d37595079385a8baf2f6f6a9a8
Soptareanu <soptareanu_alex@yahoo.com>
2025-09-10 16:15:41 +0300

    SIP-000. Bug fix. Remove deprecated code from prev. version that cause issue at installation.

--- samedaycourier.php
+++ samedaycourier.php
@@ -112,7 +112,7 @@ class SamedayCourier extends CarrierModule
         $this->name = 'samedaycourier';
         $this->tab = 'shipping_logistics';
 
-        $this->version = '1.8.0';
+        $this->version = '1.8.1';
         $this->author = 'Sameday Courier';
         $this->need_instance = 0;
         $this->bootstrap = true;
--- upgrade/upgrade-1.8.1.php
+++ upgrade/upgrade-1.8.1.php
@@ -19,11 +19,11 @@ if (!defined('_PS_VERSION_')) {
 }
 
 /**
- * This function updates your module from previous versions to the version 1.8.0,
+ * This function updates your module from previous versions to the version 1.8.1,
  * useful when you modify your database, or register a new hook ...
  * Don't forget to create one file per version.
  */
-function upgrade_module_1_8_0($object)
+function upgrade_module_1_8_1($object)
 {
     $samedayCityTableName = _DB_PREFIX_ . SamedayCity::TABLE_NAME;
     if (false === (new SamedayGeneralQueryHelper())->isTableExists($samedayCityTableName)) {
